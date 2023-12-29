f5cab3cecdf39ba278b40701a2d3f5a5b2bc73a9
MrBartosz <maksimowski97@gmail.com>
2023-12-29 15:37:30 +0100

    feat: add page.tsx and about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import React from 'react'
-import Link from 'next/link'
-
-export default function Home() {
-  return (
-    <main className='flex justify-center bg-green-500'>
-      <Link
-        className='p-3 text-xl text-white'
-        href='/home'
-      >
-        signIn
-      </Link>
-    </main>
-  )
+const Home = () => {
+  return <div>Hello!</div>
 }
+
+export default Home
