ca3c3e7100ca1408dc9526452e3127c5a5ffcbb9
MrBartosz <maksimowski97@gmail.com>
2023-12-29 13:16:53 +0100

    eslint-prettier-added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1 className={`mb-3 text-2xl font-semibold`}>Hi</h1>;
+  return (
+    <main className='flex justify-center bg-green-500'>
+      <Link
+        className='p-3 text-xl text-white'
+        href='/home'
+      >
+        signIn
+      </Link>
+    </main>
+  )
 }
