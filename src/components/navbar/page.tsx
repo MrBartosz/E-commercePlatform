edae15c8260bff69d02377939c0a2672f4eb1c3a
MrBartosz <maksimowski97@gmail.com>
2024-01-04 13:35:15 +0100

    feat: user logout with message + redirect to homepage

--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -1,13 +1,43 @@
-import Links from './links/Links'
+'use client'
+import React from 'react'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Swal from 'sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 const Navbar = () => {
+  const { data: session } = useSession()
+
+  const handleSignOut = async () => {
+    showSuccessNotification('Logout Successful !')
+    setTimeout(async () => {
+      await signOut()
+    }, 600)
+  }
+
+  const handleSignIn = async () => {
+    await signIn()
+  }
+
+  const showSuccessNotification = (message: string) => {
+    Swal.fire({
+      icon: 'success',
+      title: message,
+      showConfirmButton: false,
+      timer: 1500,
+    })
+  }
+
   return (
-    <div>
-      <div>Logo</div>
-      <div>
-        <Links />
-      </div>
-    </div>
+    <>
+      {session ? (
+        <div className='flex gap-20'>
+          <p>Welcome {session?.user?.name}</p>
+          <button onClick={handleSignOut}>Sign Out</button>
+        </div>
+      ) : (
+        <button onClick={handleSignIn}>Sign In</button>
+      )}
+    </>
   )
 }
 
