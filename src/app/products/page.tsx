96a5cfd25a7e785eba1d55d403d0976dc2be342f
MrBartosz <maksimowski97@gmail.com>
2024-01-14 02:18:48 +0100

    feat: implement pagination for product listing

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,17 +1,39 @@
 'use client'
 
+import React, { useState } from 'react'
 import SingleProduct from '@/components/products/singleProduct'
-import { mockProduct } from '../api/auth/mocks/mocks'
+import { mockProducts } from '../api/auth/mocks/mocks'
+import Pagination from './pagination'
 
 const ProductsPage = () => {
+  const [currentPage, setCurrentPage] = useState(1)
+  const productsPerPage = 20
+
+  const indexOfLastProduct = currentPage * productsPerPage
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage
+  const currentProducts = mockProducts.slice(indexOfFirstProduct, indexOfLastProduct)
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber)
+  }
+
   return (
-    <div className='flex flex-wrap justify-center mx-auto w-4/5'>
-      <SingleProduct product={mockProduct} />
-      <SingleProduct product={mockProduct} />
-      <SingleProduct product={mockProduct} />
-      <SingleProduct product={mockProduct} />
-      <SingleProduct product={mockProduct} />
-    </div>
+    <>
+      <div className='flex flex-wrap justify-center mx-auto lg:w-3/4'>
+        {currentProducts.map((product) => (
+          <SingleProduct
+            key={product.id}
+            product={product}
+          />
+        ))}
+      </div>
+      <Pagination
+        productsPerPage={productsPerPage}
+        totalProducts={mockProducts.length}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </>
   )
 }
 
